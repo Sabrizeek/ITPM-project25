0bccb7a0ab64b7fdc92ff57d3ceb50a1d6dfba70
Pamod Pushpamal <145574206+PamodN@users.noreply.github.com>
2025-04-27 19:13:52 +0530

    frontend admin page

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home/Home';
+
+import React from 'react';
+
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import Admin from './Components/Admin/Admin';
+
+
+
+function App() {
+  return (
+    <div>
+
+      
+   
+       <React.Fragment>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/mainhome" element={<Home/>}/>
+        
+           
+        <Route path="/home2" element={<Admin/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        </Routes>
+       </React.Fragment>
+        
+    </div>
+  );
+}
+
+export default App;
