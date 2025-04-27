fa2c686e04d86c9642288c263fe95d9974b2781e
AJWARDRIFAQ <127876520+AJWARDRIFAQ@users.noreply.github.com>
2025-04-27 18:49:34 +0530

    backend index

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ dotenv.config(); // Load environment variables before using them
 import express from "express";
 import { connectDB } from "./config/db.js";
 import cors from "cors"; 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,18 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/message", messageRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 console.log("Mongo URI:", process.env.MONGO_URI); // Debugging line
 
