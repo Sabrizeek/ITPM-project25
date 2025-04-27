c38d2da674ed1df98a8e92b451f421ab81e3bfca
Sabrizeek <134320747+Sabrizeek@users.noreply.github.com>
2025-04-27 14:01:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import CalendarComponent from './components/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
+      <div style={{ maxWidth: '1200px', margin: '0 auto' }}>
+        <CalendarComponent />
+      </div>
+    </ConfigProvider>
   );
 }
 
