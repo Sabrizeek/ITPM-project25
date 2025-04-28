3b03897ed6a36a188abd668789dde91c16fbb028
sandali <sandaliwathsalavi25@gmail.com>
2025-04-28 14:53:03 +0530

    update contactapi file

--- frontend/src/services/contactApi.js
+++ frontend/src/services/contactApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/contacts";
+const BASE_URL = `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/api/contacts`;
 
 // Create a new contact
 export const createContact = async (contactData) => {
