3dfe24034ebd8be4652d5be56c36852a0ffde04b
Sabrizeek <134320747+Sabrizeek@users.noreply.github.com>
2025-04-28 16:07:52 +0530

    update live chat

--- frontend/src/Components/Nav/NavHome.js
+++ frontend/src/Components/Nav/NavHome.js
@@ -16,7 +16,7 @@ function NavHome() {
       <ul className="navbar-links">
         <li><Link to="/mainhome" className="nav-link">Home</Link></li>
              <li><Link to="/profile" className="nav-link">Contacts</Link></li>
-               <li><Link to="/dashboard" className="nav-link">Live chats</Link></li>
+               <li><Link to="/app" className="nav-link">Live chats</Link></li>
                <li><Link to="/Calendar" className="nav-link">Follow-Ups</Link></li>
         <li>
           <button onClick={handleLogout} className="logout-button">Logout</button>
